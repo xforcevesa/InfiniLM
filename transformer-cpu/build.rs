bcc8b68853d405572d9952be1e4086276a3a98b9
YdrMaster <ydrml@hotmail.com>
2024-03-21 10:02:05 +0800

    refactor(transformer-cpu): 开启 mkl 支持，自动检测
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- transformer-cpu/build.rs
+++ transformer-cpu/build.rs
@@ -1,4 +1,7 @@
 ﻿fn main() {
-    // println!("cargo:rustc-cfg=detected_mkl");
-    // println!("cargo:rustc-link-lib=dylib=mkl_rt");
+    if !intel_mkl_tool::Library::available().is_empty() {
+        // **NOTICE** add mkl_rt to library search path
+        println!("cargo:rustc-cfg=detected_mkl");
+        println!("cargo:rustc-link-lib=dylib=mkl_rt");
+    }
 }
