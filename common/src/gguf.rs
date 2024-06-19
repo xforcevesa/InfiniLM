b5dd537688410372fbb3e271d40a92b6066d13dd
xforcevesa <nomodeset@qq.com>
2024-06-19 10:53:07 +0800

    Added load_from_dir, bug fixed on Serialize & Deserialize

--- common/src/gguf.rs
+++ common/src/gguf.rs
@@ -6,9 +6,60 @@ use std::{collections::{hash_map, HashMap}, fs::File, mem::size_of_val, ops::Der
 
 use crate::FileLoadError;
 
+/// 张量信息。
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct GgufTensor {
+    /// 张量名称。
+    pub name: String,
+    /// 张量类型。
+    pub kind: u32,
+    /// 偏移量。
+    pub offset: u64,
+    /// 大小。
+    pub size: u64,
+    /// shape is the number of elements in each dimension
+    pub shape: Vec<u64>,
+}
+
+impl GgufTensor {
+    /// 创建张量信息。
+    pub fn new(name: String, kind: u32, offset: u64, size: u64, shape: Vec<u64>) -> Self {
+        Self {
+            name,
+            kind,
+            offset,
+            size,
+            shape,
+        }
+    }
+
+    /// 从 [Tensor] 转换为 [GgufTensor]。
+    pub fn from(tensor: &Tensor) -> Self {
+        Self {
+            name: tensor.name.clone(),
+            kind: tensor.kind,
+            offset: tensor.offset,
+            size: tensor.size,
+            shape: tensor.shape.clone(),
+        }
+    }
+}
+
+impl Into<GgufTensor> for Tensor {
+    fn into(self) -> GgufTensor {
+        GgufTensor {
+            name: self.name,
+            kind: self.kind,
+            offset: self.offset,
+            size: self.size,
+            shape: self.shape,
+        }
+    }
+}
+
 /// gguf 文件的统一结构。
 pub struct Ggufs {
-    tensors: HashMap<String, (usize, Tensor)>, // name -> (file_index, tensor_info)
+    tensors: HashMap<String, (usize, GgufTensor)>, // name -> (file_index, tensor_info)
     files: Vec<(Mmap, String)>,                    // file_index -> (mmap, format)
 }
 
@@ -28,21 +79,56 @@ pub struct Gguf<'a> {
 /// [Ggufs] 的张量迭代器。
 pub struct Iter<'a> {
     obj: &'a Ggufs,
-    iter: hash_map::Iter<'a, String, (usize, Tensor)>,
+    iter: hash_map::Iter<'a, String, (usize, GgufTensor)>,
 }
 
 /// 共享的张量。
 #[derive(Clone)]
 pub struct SharedGguf {
     ggufs: Pin<Arc<Ggufs>>,
-    value: &'static (usize, Tensor),
+    value: &'static (usize, GgufTensor),
     data: &'static [u8],
 }
 
 impl Ggufs {
     /// 自动从路径中加载 gguf 文件。
-    pub fn load_from_dir(_path: impl AsRef<Path>) -> Result<Self, FileLoadError> {
-        Err(FileLoadError::Io(std::io::Error::new(std::io::ErrorKind::InvalidInput, "Not supported yet")))
+    pub fn load_from_dir(path: impl AsRef<Path>) -> Result<Self, FileLoadError> {
+        // 寻找path目录下所有文件
+        let mut files = Vec::new();
+        let mut tensors = HashMap::new();
+        for entry in std::fs::read_dir(path).unwrap() {
+            let entry = entry.unwrap();
+            let path = entry.path();
+            if path.is_file() {
+                let path_str = path.to_str().unwrap();
+                // 判断后缀名
+
+                if let Some(ext) = path_str.rsplit('.').next() {
+                    if ext != "gguf" {
+                        // 加载gguf文件
+                        continue;
+                    }
+                } else {
+                    continue;
+                }
+                // 加载gguf文件
+                let file = File::open(path.clone()).unwrap();
+                let file = unsafe { Mmap::map(&file) }.unwrap();
+                let mut container = match get_gguf_container(path_str) {
+                    Ok(container) => container,
+                    Err(e) => return Err(FileLoadError::Io(std::io::Error::new(std::io::ErrorKind::InvalidData, e))),
+                };
+                let model = match container.decode() {
+                    Ok(container) => container,
+                    Err(e) => return Err(FileLoadError::Io(std::io::Error::new(std::io::ErrorKind::InvalidData, e))),
+                };
+                model.tensors().into_iter().for_each(|t|{
+                    tensors.insert(t.name.clone(), (files.len(), t.clone().into()));
+                });
+                files.push((file, String::from("gguf")));
+            }
+        };
+        Ok(Self { tensors, files })
     }
 
     /// 从单个 gguf 文件中加载张量。
@@ -58,7 +144,7 @@ impl Ggufs {
         };
         let mut tensors = HashMap::new();
         model.tensors().into_iter().for_each(|t|{
-            tensors.insert(t.name.clone(), (0, t.clone()));
+            tensors.insert(t.name.clone(), (0, t.clone().into()));
         });
         let file = File::open(path).unwrap();
         let file = unsafe { Mmap::map(&file) }.unwrap();
@@ -125,7 +211,7 @@ impl Ggufs {
         }
     }
 
-    fn get_internal<'a>(&'a self, i: usize, info: &'a Tensor) -> Gguf<'a> {
+    fn get_internal<'a>(&'a self, i: usize, info: &'a GgufTensor) -> Gguf<'a> {
         let (file, format) = &self.files[i];
         let header_len = unsafe { *file.as_ptr().cast::<u64>() };
         let (begin, end) = (info.offset, info.offset + info.size);
@@ -209,7 +295,7 @@ pub struct GgufsIndexMetadata {
 #[derive(serde::Serialize, serde::Deserialize, Debug)]
 pub struct GgufsHeader {
     #[serde(flatten)]
-    pub tensors: HashMap<String, Tensor>,
+    pub tensors: HashMap<String, GgufTensor>,
     #[serde(rename = "__metadata__", default = "default_metadata")]
     pub metadata: GgufsHeaderMetadata,
 }
@@ -227,7 +313,8 @@ pub struct GgufsHeaderMetadata {
     pub format: String,
 }
 
-fn load_header(file: &Mmap) -> Result<GgufsHeader, FileLoadError> {
+/// 加载 gguf 文件头。
+pub fn load_header(file: &Mmap) -> Result<GgufsHeader, FileLoadError> {
     let header_len = unsafe { *file.as_ptr().cast::<u64>() };
     let header = &file[size_of_val(&header_len)..][..header_len as _];
     serde_json::from_slice(header).map_err(FileLoadError::Json)
@@ -235,18 +322,25 @@ fn load_header(file: &Mmap) -> Result<GgufsHeader, FileLoadError> {
 
 #[test]
 fn test() {
-    let Some(model_dir) = crate::test_model::find() else {
+    let Some(model_path) = crate::test_model::find() else {
         return;
     };
-    let safetensors = match Ggufs::load_from_dir(model_dir) {
+    let ggufs = match Ggufs::single_file(model_path.clone()) {
         Ok(s) => s,
         Err(FileLoadError::Io(e)) if e.kind() == std::io::ErrorKind::NotFound => return,
-        Err(e) => panic!("{e:?}"),
+        Err(_) => {
+            let ggufs = match Ggufs::load_from_dir(model_path) {
+                Ok(s) => s,
+                Err(FileLoadError::Io(e)) if e.kind() == std::io::ErrorKind::NotFound => return,
+                Err(e) => panic!("{e:?}"),
+            };
+            ggufs
+        },
     };
     println!(
         "found {} tensors in {} files",
-        safetensors.tensors_count(),
-        safetensors.files_count(),
+        ggufs.tensors_count(),
+        ggufs.files_count(),
     );
 }
 
